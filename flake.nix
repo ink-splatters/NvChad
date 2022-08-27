53e8c1a3d3865416130de31214d25de396481d6c
Peter A <ink.splatters@pm.me>
2022-08-27 03:57:57 +0200

    added flake file

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    flake-parts.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit self; } {
+      imports = [
+        # To import a flake module
+        # 1. Add foo to inputs
+        # 2. Add foo as a parameter to the outputs function
+        # 3. Add here: foo.flakeModule
+
+      ];
+      systems = nixpkgs.lib.systems.flakeExposed; 
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        packages.default = with pkgs; [
+		nvim
+		llvmPackages_14.stdenv
+		];
+      };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+
+      };
+    };
+}
