ee33fdb2b9f598228410a860e2d0cbce9cb02df2
Peter A <ink.splatters@pm.me>
2022-08-27 13:28:53 +0200

    nix develop works

--- flake.nix
+++ flake.nix
@@ -2,37 +2,53 @@
   description = "Description for the project";
 
   inputs = {
-    flake-parts.inputs.nixpkgs.follows = "nixpkgs";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.inputs.nixpkgs.follows = "nixpkgs";
+
+    treefmt-flake = {
+      url = "github:srid/treefmt-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+
+    };
   };
 
-  outputs = { self, flake-parts, nixpkgs, ... }:
+  outputs = { self, flake-parts, nixpkgs, treefmt-flake, ... }:
     flake-parts.lib.mkFlake { inherit self; } {
-      imports = [
-        # To import a flake module
-        # 1. Add foo to inputs
-        # 2. Add foo as a parameter to the outputs function
-        # 3. Add here: foo.flakeModule
 
-      ];
-      systems = nixpkgs.lib.systems.flakeExposed; 
-      perSystem = { config, self', inputs', pkgs, system,  ... }: {
-        
-	buildInputs = with pkgs; [
-		neovim
-		git
-		nixpkgs-fmt
-		(pkgs.nerdfonts.override { fonts = [ "FiraCode" "Hack" ]; })
-		
-	];
+      imports = [ treefmt-flake.flakeModule ];
+      systems = nixpkgs.lib.systems.flakeExposed;
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
 
-	apps.default = let inherit system; in {
-			
-		default = let inherit system; in
-			 
+        # Provided by treefmt-flake.flakeModule
+        treefmt.formatters = {
+          inherit (pkgs)
+            nixpkgs-fmt;
+        };
 
-		nixpkgs-fmt
-	};
 
+        devShells = {
+
+          default = pkgs.mkShell {
+            buildInputs = (with pkgs; [
+              neovim
+              git
+              nixpkgs-fmt
+              direnv
+              (pkgs.nerdfonts.override { fonts = [ "FiraCode" "Hack" ]; })
+
+            ] ++ config.treefmt.buildInputs);
+
+
+            shellHook = ''
+              export PS1="\n\[\033[1;32m\][nix-shell:\w]\$\[\033[0m\] "
+              direnv allow .
+                            		  '';
+
+
+          };
+
+        };
+
+      };
     };
 }
